104543c02f573d7b109fcbce4deea3e60a66682e
imtianx <imtianx@gmail.com>
2018-05-15 23:23:30 +0800

    add project

---
+++ app/src/main/java/cn/imtianx/tx_and_learning_repo/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.imtianx.tx_and_learning_repo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
