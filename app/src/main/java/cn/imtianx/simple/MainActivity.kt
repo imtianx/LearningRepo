0d0611faf6dd73a65fc03eadfc7e4ea079fbba28
imtianx <imtianx@gmail.com>
2018-05-30 20:06:39 +0800

    update

--- app/src/main/java/cn/imtianx/simple/MainActivity.kt
+++ app/src/main/java/cn/imtianx/simple/MainActivity.kt
@@ -1,20 +1,22 @@
 package cn.imtianx.simple
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import cn.imtianx.jetpacklearning.common.base.BaseDataBindingActivity
+import cn.imtianx.simple.databinding.ActivityMainBinding
 import cn.imtianx.simple.ui.databinding.DataBindingActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseDataBindingActivity<ActivityMainBinding>() {
+    override fun getContentLayoutId(): Int {
+        return R.layout.activity_main
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+    override fun initWidget() {
+        super.initWidget()
         btn_data_binding.setOnClickListener {
             startActivity(Intent(this@MainActivity,
                     DataBindingActivity::class.java))
         }
     }
+
 }
