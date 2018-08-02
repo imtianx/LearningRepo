825e70fe29f34eabc48daa92fffe0ed13726928c
imtianx <imtianx@gmail.com>
2018-08-02 23:06:19 +0800

    add bottomsheet behavior

--- mdsimple/src/main/java/cn/imtianx/mdsimple/bottomsheet/BottomSheetDialogActivity.kt
+++ mdsimple/src/main/java/cn/imtianx/mdsimple/bottomsheet/BottomSheetDialogActivity.kt
@@ -5,12 +5,14 @@ import android.content.Intent
 import android.graphics.Color
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.BottomSheetDialog
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import cn.imtianx.mdsimple.R
 import cn.imtianx.mdsimple.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_bottom_sheet.*
 import kotlinx.android.synthetic.main.dialog_bottom_sheet_camera.view.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
@@ -27,6 +29,73 @@ class BottomSheetDialogActivity : BaseActivity() {
 
     override fun getContentLayoutId(): Int {
         return R.layout.activity_bottom_sheet
+    }
+
+    override fun initWidget() {
+        super.initWidget()
+
+        btn_bottom_sheet_list.setOnClickListener {
+            showBottomSheetDialogList()
+        }
+
+
+        // bottom sheet
+        val sheetBehavior = BottomSheetBehavior.from(sheetView)
+        // 设置下滑时是否可以被隐藏，默认false,注意若设置为true,下滑关闭 shettView时其状态为 STATE_HIDDEN，否则为 STATE_COLLAPSED
+        sheetBehavior.isHideable = true
+
+        // 折叠后的高度
+//        sheetBehavior.peekHeight = BottomSheetBehavior.PEEK_HEIGHT_AUTO
+
+
+        // set state change listener
+        sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                Log.e("tx", "newState:  $newState")
+//                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+//                    sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//                }
+
+            }
+
+        })
+
+        btn_bottom_sheet.setOnClickListener {
+
+            // 1.
+            if (sheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                // 注意：使用该属性必须设置 isHideable = true
+                sheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+
+            // 2.若设置了 isHideable = true 需要在 onStateChanged中 将 STATE_HIDDEN 设置为 STATE_COLLAPSED
+//            if (sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+//                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//            } else if (sheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+//                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+//            }
+        }
+
+    }
+
+
+    // 模拟列表
+    private fun showBottomSheetDialogList() {
+        dismissDialog()
+        bottomSheetDialog = BottomSheetDialog(this)
+        val dialogContentView = layoutInflater.inflate(R.layout.dialog_bottom_sheet_list, null)
+
+        bottomSheetDialog?.apply {
+            setContentView(dialogContentView)
+            setCancelable(true)
+            setCanceledOnTouchOutside(true)
+            show()
+        }
     }
 
 
@@ -46,6 +115,7 @@ class BottomSheetDialogActivity : BaseActivity() {
     }
 
 
+    // 显示选择 对话框
     private fun showBottomSheetDialogCamera() {
 
         dismissDialog()
