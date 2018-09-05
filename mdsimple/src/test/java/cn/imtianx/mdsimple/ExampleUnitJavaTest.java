43f846246c903f816c078ac310ed4eeec72f7eb4
imtianx <imtianx@gmail.com>
2018-09-05 14:28:30 +0800

    add test deps

---
+++ mdsimple/src/test/java/cn/imtianx/mdsimple/ExampleUnitJavaTest.java
@@ -0,0 +1,24 @@
+package cn.imtianx.mdsimple;
+
+import org.junit.Test;
+
+/**
+ * <pre>
+ *     @desc:
+ * </pre>
+ *
+ * @author 奚岩
+ * @date 2018/9/5 11:23 AM
+ */
+public class ExampleUnitJavaTest {
+
+    @Test
+    public void test() {
+        System.out.println(0x0001 | 0x0002);
+        System.out.println(0x0001<<0);
+        System.out.println(0x0001<<4);
+        System.out.println(48|1);
+        System.out.println(16|1);
+
+    }
+}
